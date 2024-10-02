7b932ea450dcc63ce54ff8fc275d22852d9d7f9b
Carlos Sanchez <c.sturbides@gmail.com>
2024-10-02 00:58:19 -0400

    Updated backend with boilerplate

---
+++ backend/src/common/abstract.entity.ts
@@ -0,0 +1,49 @@
+import {
+  Column,
+  CreateDateColumn,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+import { LanguageCode } from '../constants';
+import type { AbstractDto, AbstractTranslationDto } from './dto/abstract.dto';
+
+export abstract class AbstractEntity<
+  DTO extends AbstractDto = AbstractDto,
+  O = never,
+> {
+  @PrimaryGeneratedColumn('uuid')
+  id!: Uuid;
+
+  @CreateDateColumn({
+    type: 'timestamp',
+  })
+  createdAt!: Date;
+
+  @UpdateDateColumn({
+    type: 'timestamp',
+  })
+  updatedAt!: Date;
+
+  translations?: AbstractTranslationEntity[];
+
+  toDto(options?: O): DTO {
+    const dtoClass = this.constructor.prototype.dtoClass;
+
+    if (!dtoClass) {
+      throw new Error(
+        `You need to use @UseDto on class (${this.constructor.name}) be able to call toDto function`,
+      );
+    }
+
+    return new dtoClass(this, options);
+  }
+}
+
+export class AbstractTranslationEntity<
+  DTO extends AbstractTranslationDto = AbstractTranslationDto,
+  O = never,
+> extends AbstractEntity<DTO, O> {
+  @Column({ type: 'enum', enum: LanguageCode })
+  languageCode!: LanguageCode;
+}
